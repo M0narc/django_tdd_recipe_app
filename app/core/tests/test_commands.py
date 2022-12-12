5416fd79b5c249a15c67571c49e3b69dcdaa63b9
M0narc <francomatiaslujan23@gmail.com>
2022-12-12 05:46:13 -0300

    checks.yml updated, wait_for_db completed, docker-compose updated with steps to run wait_for_db and migrate first before starting server

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,6 +11,7 @@ from django.db.utils import OperationalError
 # base test class, we are testing "when DB is not available"
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test Commands."""
@@ -21,15 +22,15 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
-    
+        patched_check.assert_called_once_with(databases=['default'])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patch_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-            
-        call_command['wait_for_db']
+ 
+        call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
