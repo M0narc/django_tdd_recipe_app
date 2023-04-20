de5a417da0b419ff3c41f55bf87cc6682f18aff0
M0narc <francomatiaslujan23@gmail.com>
2023-04-20 20:57:12 -0300

    token tests in test_user_api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,6 +12,7 @@ from rest_framework import status
 # app and create as an endpoint
 # this will return the full url for our proyect
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 # Helper function time!
@@ -63,3 +64,42 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(email=payload["email"])
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """test Generates token for valid credentials."""
+        user_details = {
+            'name': 'John Doe',
+            'email': 'test@example.com',
+            'password': 'Test123'
+        }
+        create_user(**user_details)
+
+        payload = {
+            'email': user_details['email'],
+            'password': user_details['password'],
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid."""
+        create_user(email='test@example.com', password='Test123')
+
+        payload = {'email': 'test@example', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_bad_password(self):
+        """Test posting a bad password returns error"""
+        payload = {
+            'email': 'test@example',
+            'password': ''
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
